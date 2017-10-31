99af1706827a88f6f66c2588d357af3ea4318ea2
melcheg <33259155+melcheg@users.noreply.github.com>
2017-10-31 18:30:37 +0200

    Добавил комментарий

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 700, 175));
         primaryStage.show();
     }
-
+// skvnk;dvnkwnvirevnirehvulerhvhlervreger
 
     public static void main(String[] args) {
         launch(args);
